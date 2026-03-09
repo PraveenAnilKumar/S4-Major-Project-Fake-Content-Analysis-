88bb215c80aefb1bc4343991d898a341c87731ad
PraveenAnilKumar <aspraveen67@gmail.com>
2026-03-09 20:44:31 +0530

    Delete test_load.py

--- test_load.py
+++
@@ -1,6 +0,0 @@
-from fake_news_detector import fake_news_detector
-try:
-    fake_news_detector.load_models('models/fake_news/')
-    print("Transformer loaded:", fake_news_detector.use_transformer)
-except Exception as e:
-    print("Exception during load_models:", e)
